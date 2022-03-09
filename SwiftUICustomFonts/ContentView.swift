1ae5947abd445f3c869ad1f29112ea0b031a80b1
Jeremy Gale <jeremy.gale@shift.com>
2022-03-09 14:26:30 -0700

    Initial Commit

---
+++ SwiftUICustomFonts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICustomFonts
+//
+//  Created by Jeremy Gale on 03/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUICustomFonts/SwiftUICustomFontsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICustomFontsApp.swift
+//  SwiftUICustomFonts
+//
+//  Created by Jeremy Gale on 03/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICustomFontsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
